f46755959ab73bb2cd3e3cc43875ca685dcc24bc
eddie <bda605@gmail.com>
2019-02-14 21:39:22 +0800

    初始化專案

---
+++ ConsoleApp1/Operation.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    public class Operation
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+
+        public int Sub()
+        {
+            return 0;
+        }
+    }
+}
